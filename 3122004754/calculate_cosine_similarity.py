2e3ddc3b688c967db27d69b74e49881c0064b748
vvvvv19 <1009957030@qq.com>
2024-09-12 02:16:36 +0800

    creat calculate_cosine_similarity.py  updata main.py 创建calculate_cosine_similarity.py 由于上次使用jaccard测试结果并不理想，改用计算余弦相似度和jaccard相似度将二者以0.8余弦相似度与0.2jaccard相似度的权值相加得到最终的查重率，该模块实现了余弦相似度的计算。 更新main函数，使其能完成上述功能。 更新测试输出文件output_0.8_add.txt结果为0.77，结果较好。

---
+++ 3122004754/calculate_cosine_similarity.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def calculate_cosine_similarity(text1, text2):  # 计算余弦相似度
+    # 初始化 TF-IDF 向量化器
+    vectorizer = TfidfVectorizer()
+
+    # 将文本转化为 TF-IDF 矩阵
+    # fit_transform() 方法会学习文本数据的词汇表并将文本转换为 TF-IDF 矩阵
+    # 这里的 [text1, text2] 是一个文本列表
+    tfidf_matrix = vectorizer.fit_transform([text1, text2])
+
+    # 计算两个文本之间的余弦相似度
+    # cosine_similarity() 方法接受两个矩阵作为输入，这里分别是 tfidf_matrix[0:1] 和 tfidf_matrix[1:2]
+    # tfidf_matrix[0:1] 代表第一个文本的 TF-IDF 向量
+    # tfidf_matrix[1:2] 代表第二个文本的 TF-IDF 向量
+    # 计算结果是一个 1x1 的矩阵，包含两个文本之间的余弦相似度值
+    # [0][0] 提取这个相似度值
+    return cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
--- 3122004754/main.py
+++ 3122004754/main.py
@@ -2,6 +2,7 @@ import sys
 import rw_file as rw
 import preprocess_text as pre
 import calculate_jaccard_similarity as jacsim
+import calculate_cosine_similarity as cossim
 
 def main():
     if len(sys.argv) != 4:  # 确保用户按照正确的格式提供了所需的文件路径，否则终止运行
@@ -23,8 +24,14 @@ def main():
 
     # 计算 Jaccard 相似度
     jaccard_similarity = jacsim.calculate_jaccard_similarity(original_text_processed, plagiarized_text_processed)
-    rw.write_result(output_file, jaccard_similarity)
-    print(jaccard_similarity)
+    # 计算余弦相似度
+    cosine_similarity_score = cossim.calculate_cosine_similarity(original_text_processed, plagiarized_text_processed)
+
+    #综合两者的结果，通过加权将两种相似度结果结合起来，以获得综合相似度评分
+    #这里Jaccard 相似度权重为 20%，余弦相似度权重为 80%
+    all_score=0.2*jaccard_similarity+cosine_similarity_score*0.8
+    print(all_score)
+    rw.write_result(output_file,all_score)
 
 #执行main函数
 if __name__ == "__main__":
