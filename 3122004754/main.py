cd561dad894c7ddc1e3e0d6e3f812ea7db3615ea
vvvvv19 <1009957030@qq.com>
2024-09-11 22:16:41 +0800

    updata main.py 更新main.py 对库函数进行更新，删去不需要的库，引用其他模块的函数。 创建main函数，函数内容如下： 首先对比sys.argv长度是否时4，确保用户按照正确的格式提供了所需的文件路径，否则终止运行。 后要求用户通过命令行导入文本。 使用rw_file模块的函数读取文本。 使用pre.preprocess_text函数对文本进行预处理。 计算jaccard相似度，输出到指定文本中，并打印。

--- 3122004754/main.py
+++ 3122004754/main.py
@@ -1,5 +1,31 @@
 import sys
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import rw_file as rw
+import preprocess_text as pre
+import calculate_jaccard_similarity as jacsim
 
+def main():
+    if len(sys.argv) != 4:  # 确保用户按照正确的格式提供了所需的文件路径，否则终止运行
+        print("Usage: python script.py <original_file> <plagiarized_file> <output_file>")
+        sys.exit(1)
+
+    # 命令行导入文本
+    original_file = sys.argv[1]
+    plagiarized_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    # 读取文件内容
+    original_text = rw.read_file(original_file)
+    plagiarized_text = rw.read_file(plagiarized_file)
+
+    # 文本预处理
+    original_text_processed = pre.preprocess_text(original_text)
+    plagiarized_text_processed = pre.preprocess_text(plagiarized_text)
+
+    # 计算 Jaccard 相似度
+    jaccard_similarity = jacsim.calculate_jaccard_similarity(original_text_processed, plagiarized_text_processed)
+    rw.write_result(output_file, jaccard_similarity)
+    print(jaccard_similarity)
+
+#执行main函数
+if __name__ == "__main__":
+    main()
