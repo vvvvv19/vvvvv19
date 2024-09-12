2ef58734932cc7ab3e532760ef43aea1256bed9b
vvvvv19 <1009957030@qq.com>
2024-09-13 03:12:17 +0800

    updata preprocess_text.py 在创建集合时使用了不必要的 set() 函数嵌套，改成直接用集合字面量 {} 来定义集合，无需 set() 函数

--- 3122004754/preprocess_text.py
+++ 3122004754/preprocess_text.py
@@ -2,16 +2,15 @@ import jieba
 import string
 
 # 停用词列表（示例，实际使用时可以根据需求调整）
-stop_words = set(['的', '了', '是', '很', '我', '有', '和', '也','你','们','他','她'])
+stop_words = {'的', '了', '是', '很', '我', '有', '和', '也', '你', '们', '他', '她'}
+
 
 def preprocess_text(text):
-    # 去除标点符号
-    words = jieba.cut(text)
-    punctuation = string.punctuation + "，。！？；：“”‘’（）【】《》"
-    text = ''.join(word for word in words if word not in punctuation)
-    # 分词
-    words = jieba.lcut(text)
-   # 去除停用词
-    words = [word for word in words if word not in stop_words]
-    # 返回以空格分隔的词汇字符串
-    return ' '.join(words)
+        # 定义标点符号集合
+        punctuation = set(string.punctuation + "，。！？；：“”‘’（）【】《》")
+        # 分词
+        words = jieba.lcut(text)
+        # 去除标点符号和停用词
+        filtered_words = [word for word in words if word not in punctuation and word not in stop_words]
+        # 返回以空格分隔的词汇字符串
+        return ' '.join(filtered_words)
