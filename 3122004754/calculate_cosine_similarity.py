31a591df39d67b34d90c5d0264b510068aa53006
vvvvv19 <1009957030@qq.com>
2024-09-13 20:26:54 +0800

    update calculate_cosine_similarity.py 更新余弦相似度模块，设定阈值，避免因精度问题结果大于1的情况。

--- 3122004754/calculate_cosine_similarity.py
+++ 3122004754/calculate_cosine_similarity.py
@@ -16,4 +16,4 @@ def calculate_cosine_similarity(text1, text2):  # 计算余弦相似度
     # tfidf_matrix[1:2] 代表第二个文本的 TF-IDF 向量
     # 计算结果是一个 1x1 的矩阵，包含两个文本之间的余弦相似度值
     # [0][0] 提取这个相似度值
-    return cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    return min(max(cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0], 0.0), 1.0)  # 确保结果在 [0, 1] 范围内,避免因精度问题结果大于1的情况
